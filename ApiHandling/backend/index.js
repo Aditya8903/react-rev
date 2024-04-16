cfaba07f93fa40cc3e70ec081aa5c4ad22b69d9c
Aditya8903 <aditya892003@gmail.com>
2024-04-16 18:19:04 +0530

    api handling mini project

---
+++ ApiHandling/backend/index.js
@@ -0,0 +1,10 @@
+import express from "./express";
+const app = express();
+app.get("/api/products", (req, res) => {
+  const products = [];
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running at port number : ${port}`);
+  g;
+});
